0f32f2e97b3911ff4d25eb166130b6bc65974f3a
Shungo KAWAMURA <t22cs011@yamanashi.ac.jp>
2024-11-15 16:00:21 +0900

    テスト項目追加

--- tests/Player_Test.py
+++ tests/Player_Test.py
@@ -1,8 +1,9 @@
 import unittest
 import Player
+
 class PlayerTest(unittest.TestCase):
     
-    def testName(selt):
+    def testName(self):
         name = "A"
         playerA = Player.Player(name)
         
@@ -12,15 +13,15 @@ class PlayerTest(unittest.TestCase):
         playerA = Player.Player("A")
         
         self.assertTrue(0 <= playerA.show_hand() <= 2)
+        self.assertFalse(playerA.show_hand() < 0)
         
     def testWincount(self):
-        playerA = Player("A")
+        playerA = Player.Player("A")
         
         playerA.notify_result(True)
         self.assertEqual(1, playerA.get_wincount())
         playerA.notify_result(False)
         self.assertEqual(1, playerA.get_wincount())
-    
     
 if __name__ == '__main__':
     #import sys:sys.argv = [‘’, ‘Test.testPlayer’]
